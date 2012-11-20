c4f04c6363ff0745dd51d79f66f15f50e50336d4
Manu S Ajith <neo@codingarena.in>
2012-11-21 03:04:14 +0530

    Solved Safety in numbers problem

---
+++ Round_1B/Safety_in_numbers/safety_in_numbers_large.rb
@@ -0,0 +1,29 @@
+input = File.new("input_large.txt")
+input.gets.to_i.times do |i|
+	datas = input.gets.split.map{|i| i.to_i}
+	num = datas[0]
+	judges = datas[1,num].map{|i| i.to_f}
+	bigs = judges.sort.reverse
+	total = judges.inject{|sum,i| sum+=i}
+	total_d = total * 2.0
+	cur = 0
+	n = num
+	ave = total_d/n
+	total_tmp = total_d
+	while(bigs[cur]>ave) do
+		total_tmp -= bigs[cur]
+		cur += 1
+		ave = total_tmp/(num-cur)
+	end
+	dsts = []
+	judges.each do |i|
+    dst = 0
+		if (i<ave) then
+			dst = ((ave-i)/total)*100.0
+		end
+		dsts.push(sprintf("%.6f", dst))
+	end
+	print 'Case #' + (i+1).to_s + ':'
+	dsts.each{|dst| print " "+dst}
+	print "\n"
+	end
